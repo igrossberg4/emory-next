abd30f1b2e21341915f446e97e1c8395431b7109
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-07-30 10:01:20 +0200

    STYLE - Add sass.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+const path = require('path')
 module.exports = {
   reactStrictMode: true,
   images: {
     loader: 'imgix',
     path: '/',
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { AnimateSharedLayout } from "framer-motion";
-import "../styles/embla.css";
+import "../styles/embla.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return<AnimateSharedLayout>
