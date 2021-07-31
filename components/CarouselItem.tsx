186e1084124cd6278753c142836cd4859a29d1fa
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-07-31 19:49:52 +0200

    Header top animations review and different animations according to the previous path.

--- components/CarouselItem.tsx
+++ components/CarouselItem.tsx
@@ -1,32 +1,32 @@
 import Head from "next/head";
 import React, { Fragment } from "react";
 import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/dist/client/router";
-import { Box, Container } from "@chakra-ui/react";
+import {Box, Container} from "@chakra-ui/react";
 import { motion } from "framer-motion";
 
 export default function CarouselItem(props: any) {
   const router = useRouter();
+  // debugger;
   return (
-    <div className="carousel-item">
-        <div className="image round-wp">
-            {/*<img alt={props.header} src={props.img_src}/>*/}
-          {/*<motion.figure transition={{delay:0.2}} layoutId={props.img_src}>*/}
-           <Image alt="{props.header}" src={props.img_src} width="530" height="530" ></Image>
-          {/*</motion.figure>*/}
-        </div>
+    <div className="carousel-item container-force-screen-fit-y">
+      <div className="content">
+        <img className="image round-wp" alt={props.header} src={props.img_src} />
         <h2 className="title">{props.header}</h2>
-        <div className="btn"
-            onClick={(e) => {
-              if (props.action && props.action.type === "navigate") {
-                router.push(props.action.route_to);
-              }
-            }}
-          >
-            {" "}
-            {props.button_scroll}
+      </div>
+        <div className="actions">
+          <Link href={{
+            pathname: props.action.route_to,
+            query: { from: 'carousel' },
+          }}
+          as={ props.action.route_to + '#carousel' }
+          shallow={false}>
+            <a className="btn">{props.button_scroll}</a>
+          </Link>
+          <div className="line-separator line-separator--half-height"></div>
+
         </div>
-        <div className="line-separator line-separator--half-height"></div>
     </div>
   );
 }
