d29d1dcd81ceb9466213b9b56859e621f6d6fb45
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-08-09 16:05:18 +0200

    MENU - Add icon button.

--- components/MenuTop.tsx
+++ components/MenuTop.tsx
@@ -18,14 +18,15 @@ import {
 } from "@chakra-ui/react";
 import { Context } from "../state/Store";
 import Link from "next/link";
+import IconButton from "./IconButton";
 
 export default function MenuTop(props:any) {
   const router = useRouter();
   const [state, dispatch] = useContext(Context) as any;
   return (
-    <div style={{position: "fixed", top:0, zIndex:10}}>
+    <div style={{position: "fixed", top:0, zIndex:10, right:0}}>
     <Menu >
-      <MenuButton as={Button}>{props.title}</MenuButton>
+      <MenuButton  as={Button} rightIcon={<IconButton icon="menu" />}>{props.title}</MenuButton>
       <MenuList>
         {props.options.map((option:any) => (
           <MenuItem
