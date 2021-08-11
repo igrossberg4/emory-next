c587243fe185a2ba77d6e33387ee3532228c6acd
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-08-11 09:00:19 +0200

    Remove google fonts dependencies.

--- pages/[[...path]].tsx
+++ pages/[[...path]].tsx
@@ -54,9 +54,6 @@ export default function Home(props: any) {
         <title>{props.meta.title}</title>
         <meta name="description" content={props.meta.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'true'} />
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
