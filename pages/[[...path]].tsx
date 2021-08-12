c3c94064337749e88dcd5e7f3d20e38e1c7fc220
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-08-12 14:45:43 +0200

    ROUTE - Base component dymmy redirect to home

--- pages/[[...path]].tsx
+++ pages/[[...path]].tsx
@@ -136,7 +136,7 @@ const introComponent = {
         video_src: "video.mp4",
         text_play: "Begin\r\nyour\r\nexperience",
         text_skip: "Skip video",
-        route_to: "/landing/carousel/first",
+        route_to: "/home",
       },
     },
   ],
