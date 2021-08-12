8aa4218aeb7d5879cff045361858b666054a5557
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-08-12 12:20:50 +0200

    NODES - Added way to make relaionships between pages and content.

--- data-loader/get-nodes.ts
+++ data-loader/get-nodes.ts
@@ -92,13 +92,15 @@ function prepareMenu(nodes: Array<any>) {
 
 function prepareBottomMenu(lastNode: any, nextNode: any, nodes: Array<any>, baseNode:any) {
     const basePath = baseNode ? `${baseNode.path}/` : '';
-    const previous_route = lastNode ? `${basePath}${lastNode.path}` : `${basePath}${nodes[0].path}`;
-    const next_route = nextNode ? `${basePath}${nextNode.path}` : `${basePath}${nodes[0].path}`;
+    const prevNodeSelect = !lastNode ? nodes[nodes.length-1] : lastNode;
+    const nextNodeSelect = !nextNode ? nodes[0] : nextNode;
+    const previous_route = prevNodeSelect.id === baseNode.id ? `${baseNode.path}` : `${baseNode.path}/${prevNodeSelect.path}`;
+    const next_route = nextNodeSelect.id === baseNode.id ? `${baseNode.path}` : `${baseNode.path}/${nextNodeSelect.path}`;
     return {
         "component": "BottomNavigation",
         "props": {
-            "previous_title": lastNode ? lastNode?.page_props?.header : nodes[0]?.page_props?.header,
-            "next_title": nextNode ? nextNode?.page_props?.header : nodes[0]?.page_props?.header,
+            "previous_title": prevNodeSelect?.page_props.header,
+            "next_title": nextNodeSelect?.page_props?.header,
             "previous_route": previous_route,
             "next_route": next_route
         }
@@ -107,20 +109,21 @@ function prepareBottomMenu(lastNode: any, nextNode: any, nodes: Array<any>, base
 
 function generatePageWithComponents(pages_list: {list:Array<string>, nodeBase:any}, nodes: Array<any>, menus: any) {
     const pages = pages_list.list;
+    const nodesForCollection = pages.map(page => nodes.find(node=> node.id === page));
     return pages.map((page, i) => {
         const nodeFinded = nodes.find(node => page === node.id);
         const prevNode = i === 0 ? null : nodes.find(node => node.id === pages[i - 1]);
         const nextNode = i === pages.length - 1 ? null : nodes.find(node => node.id === pages[i + 1]);
         console.log("NODE BASE 1", pages_list.nodeBase)
         return {
-            path: `${pages_list.nodeBase.path}/${nodeFinded.path}`,
+            path: pages_list.nodeBase.id === nodeFinded.id ? `${nodeFinded.path}` : `${pages_list.nodeBase.path}/${nodeFinded.path}`,
             meta: Object.assign({}, nodeFinded.metatag),
             view: [
                 menus,
                 {
                     component: "CarouselNavigation",
                     props: {
-                        prev: prevNode ? `${pages_list.nodeBase.path}/${prevNode.path}` : null,
+                        prev: !prevNode ? null : (prevNode.id !== pages_list.nodeBase.id ? `${pages_list.nodeBase.path}/${prevNode.path}` : pages_list.nodeBase.path),
                         actual: {
                             component: "DynamicComponentMatcher",
                             props: {
@@ -131,12 +134,12 @@ function generatePageWithComponents(pages_list: {list:Array<string>, nodeBase:an
                                     }
 
                                 ].concat(nodeFinded.components).concat(
-                                    prepareBottomMenu(prevNode, nextNode, nodes, pages_list.nodeBase)
+                                    prepareBottomMenu(prevNode, nextNode, nodesForCollection, pages_list.nodeBase)
                                     )
                             }
                         },
-                        next: nextNode ? `${pages_list.nodeBase.path}/${nextNode.path}` : null,
-                        slides: findSlides(pages, nodes, nodeFinded, prevNode, nextNode, pages_list.nodeBase)
+                        next: !nextNode ? null : (nextNode.id !== pages_list.nodeBase.id ? `${pages_list.nodeBase.path}/${nextNode.path}` : pages_list.nodeBase.path),
+                        slides: findSlides(pages, nodesForCollection, nodeFinded, prevNode, nextNode, pages_list.nodeBase)
                     }
                 }
             ]
