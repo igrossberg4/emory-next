da565ee97e3cdf2a81d79bdf74b8a5251d673317
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-08-16 10:12:41 +0200

    Issue #10: Theme component accordion

--- components/AccordionComponent.tsx
+++ components/AccordionComponent.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import React from "react";
+import React, { Fragment } from "react";
 import Image from "next/image";
 import { useRouter } from "next/dist/client/router";
 import {
@@ -25,22 +25,27 @@ import {
 export default function AccordionComponent(props: any) {
   const router = useRouter();
   return (
-    <Accordion allowToggle>
-      {props.items.map((item: any, index: number) => {
-        return (
-          <AccordionItem key={index.toString()}>
-            <h2>
-              <AccordionButton>
-                <Box flex="1" textAlign="left">
-                  {item.title}
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>{item.description}</AccordionPanel>
-          </AccordionItem>
-        );
-      })}
-    </Accordion>
+    <Fragment>
+      <div className="container accordion">
+        {props.pretitle && <p className="accordion__pretitle text-label">{props.pretitle}</p>}
+        {props.title && <h2 className="accordion__title header-h2">{props.title}</h2>}
+        <Accordion allowToggle>
+          {props.items.map((item: any, index: number) => {
+            return (
+              <AccordionItem key={index.toString()}>
+                <h3 className="accotdion-item__title text-body--lg">
+                  <AccordionButton>
+                    <Box flex="1" textAlign="left">
+                      {item.title}
+                    </Box>
+                  </AccordionButton>
+                </h3>
+                <AccordionPanel pb={4}>{item.description}</AccordionPanel>
+              </AccordionItem>
+            );
+          })}
+        </Accordion>
+      </div>
+    </Fragment>
   );
 }
