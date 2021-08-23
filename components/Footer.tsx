4b4d4ee0809ffe9c6fa9696053fedaff6b669b3d
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-08-23 13:09:04 +0200

    SCROLL - Added scroll handler to footer for hide button when is not required.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import React, { Fragment, useContext, useState } from "react";
+import React, { Fragment, useContext, useEffect, useRef, useState } from "react";
 import { useRouter } from "next/dist/client/router";
 import { Box } from "@chakra-ui/react";
 import { Context } from "../state/Store";
@@ -7,9 +7,24 @@ import Image from "next/image";
 import IconButton from "./IconButton";
 
 export default function Footer(props:any) {
+  const [scroll, setScroll] = useState(0);
+
+  const prevScrollY = useRef(0);
+  useEffect(() => {
+    const handleScroll = () => {
+      setScroll(window.scrollY);
+      const currentScrollY = window.scrollY;
+      prevScrollY.current = currentScrollY;
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: false });
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [scroll]); // @ts-ignore
   return (
     <footer role="contentinfo">
       <div className="container">
+        {scroll > 170 ? 
         <div
           className="footer__scrolltop btn text-label"
           style={{ cursor: "pointer" }}
@@ -22,7 +37,8 @@ export default function Footer(props:any) {
         >
           <IconButton icon="chevron-up"></IconButton>
           Back to top
-        </div>
+        </div> : ''
+}
         <p className="footer__title">The future starts here</p>
         <Image src="/logos/emory-university-logo.svg" alt="EMORY" width="95px" height="20px"></Image>
         <p className="footer__copyright text-label">© {new Date().getFullYear()} EMORY UNIVERSITY</p>
