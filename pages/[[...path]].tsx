45aa8281583fb8a7ea8374fcdd44aa7ec4c37d10
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-08-23 13:49:03 +0200

    META - Added og:image to metadata

--- pages/[[...path]].tsx
+++ pages/[[...path]].tsx
@@ -56,6 +56,7 @@ export default function Home(props: any) {
       <Head>
         <title>{props.meta.title}</title>
         <meta name="description" content={props.meta.description} />
+        <meta property="og:image"   content={props.meta.image}></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
           rel="apple-touch-icon"
