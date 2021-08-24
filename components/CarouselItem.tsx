22b3b62fd3802fd05056dcc07bcc9e6677b221fe
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-08-24 12:11:07 +0200

    Issue #16: Carousel header buble animation full width

--- components/CarouselItem.tsx
+++ components/CarouselItem.tsx
@@ -21,130 +21,50 @@ export default function CarouselItem(props: any) {
   const [animated, setAnimated] = useState(true);
   const [scroll, setScroll] = useState(0);
   const listInnerRef = useRef();
-
   const prevScrollY = useRef(0);
-
   const [goingUp, setGoingUp] = useState(false);
-  const [variants, setVariants] = useState({
-    mainImage_active: {
-      opacity: 1,
-      maxWidth: scroll > 170 ? "1500px" : "530px",
-      scale: scroll > 170 ? 1.5 : 1,
-      bottom: "10%",
-      transition: { duration: 4 },
-    },
-    mainImage_initialFromCarousel: {
-      opacity: 1,
-      maxWidth: "530px",
-      scale: 1,
-      bottom: "30%",
-    },
-    teaserImage_active: {
-      maxWidth: 0,
-      margin: 0,
-      border: 0,
-      transitionEnd: {
-        display: "none",
-      },
-      transition: { duration: 2 },
-    },
-    teaserImage_initialFromCarousel: {
-      maxWidth: "530px",
-    },
-    title_active: {
-      fontSize: `clamp(2em, ${normalize(scroll, 170, 0) * 3.5}em, 3.5em)`,
-      maxWidth: "780px",
-      transition: { duration: 1 },
-    },
-    title_initialFromCarousel: {
-      opacity: 1,
-      fontSize: "2em",
-      maxWidth: "650px",
-    },
-    fadeIn_active: {
-      opacity: 1,
-      transition: { duration: 2 },
-    },
-    fadeIn_initial: {
-      opacity: 0,
-    },
-  });
 
   useEffect(() => {
     const handleScroll = () => {
       setScroll(window.scrollY);
       const currentScrollY = window.scrollY;
-      setVariants({
-        mainImage_active: {
-          opacity: 1,
-          maxWidth: scroll > 170 ? "1500px" : "530px",
-
-          scale: scroll > 170 ? 1.5 : 1,
-          bottom: scroll > 170 ? "10%" : "30%",
-
-          transition: { duration: 2 },
-        },
-        mainImage_initialFromCarousel: {
-          opacity: 1,
-          maxWidth: "530px",
-          scale: 1,
-          bottom: "30%",
-        },
-        teaserImage_active: {
-          maxWidth: 0,
-          margin: 0,
-          border: 0,
-          transitionEnd: {
-            display: "none",
-          },
-          transition: { duration: 2 },
-        },
-        teaserImage_initialFromCarousel: {
-          maxWidth: "530px",
-        },
-        title_active: {
-          fontSize: `clamp(2em, ${normalize(scroll, 170, 0) * 3.5}em, 3.5em)`,
-
-          maxWidth: `520px`,
-          transition: { duration: 0.3 },
-        },
-        title_initialFromCarousel: {
-          opacity: 1,
-          fontSize: "2em",
-          maxWidth: "650px",
-        },
-        fadeIn_active: {
-          opacity: 1,
-          transition: { duration: 2 },
-        },
-        fadeIn_initial: {
-          opacity: 0,
-        },
-      });
 
       if (prevScrollY.current < currentScrollY && goingUp) {
         setGoingUp(false);
       }
+
       if (prevScrollY.current > currentScrollY && !goingUp) {
         setGoingUp(true);
       }
 
       prevScrollY.current = currentScrollY;
+
+      if (currentScrollY > 25) {
+        document.body.classList.add('is-scrolled');
+      }
+      else {
+        document.body.classList.remove('is-scrolled');
+      }
+
     };
 
     window.addEventListener("scroll", handleScroll, { passive: false });
 
     return () => window.removeEventListener("scroll", handleScroll);
-  }, [goingUp, scroll, variants]); // @ts-ignore
+  }, [goingUp, scroll]); // @ts-ignore
   return (
     // <motion.div className="content-header">
     <div className="content-header__container container-force-screen-fit-y" data-animation={props.active && scroll > 25 ? "active" : "not-active"}>
       <div className="header-inner-content">
-        <img src={props.img_src} alt={props.header} className="image round-wp"></img>
-        <div className="header-inner-content__text">
-          <div className="pretitle text-label">{props.about}</div>
-          <h1 className="title header-h2">{props.header.toUpperCase()}</h1>
-          <div className="subtitle text-body--lg">{props.text}</div>
+        <div className="wrapper">
+          <div className="header-inner-content__img">
+            <img src={props.img_src} alt={props.header} className="image"></img>
+          </div>
+          <div className="header-inner-content__text">
+            <div className="pretitle text-label">{props.about}</div>
+            <h1 className="title header-h2">{props.header.toUpperCase()}</h1>
+            <div className="subtitle text-body--lg">{props.text}</div>
+          </div>
         </div>
       </div>
       <div className="actions">
@@ -155,7 +75,7 @@ export default function CarouselItem(props: any) {
             const contentElement = document.getElementById("carouselContent");
             //contentElement?.scrollIntoView({behavior: 'smooth'});
             window.scrollTo({
-              top: contentElement?.offsetTop,
+              top: 200, //contentElement?.offsetTop,
               behavior: "smooth",
             });
           }}
