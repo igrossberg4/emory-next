e43ca5111cb0fa49266074ffcb14863f4eee5812
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-08-24 16:20:33 +0200

    TEASER - Allow teaser to connect directly to linked pages.

--- components/Teaser.tsx
+++ components/Teaser.tsx
@@ -5,13 +5,37 @@ import { Box } from "@chakra-ui/react";
 import { Context } from "../state/Store";
 import MediaWithExpantion from "./MediaWithExpantion";
 
-export default function Teaser(props:any) {
+export default function Teaser(props: any) {
+  const router = useRouter();
+  const [state, dispatch] = useContext(Context) as any;
+
   return (
     <div className="teaser">
       <img className="teaser__image round-wp" src={props.img_src}></img>
       <h3 className="teaser__title">{props.title}</h3>
       <p className="teaser__body">{props.body}</p>
-      <a href="#" className="teaser__link text-cta">Explore</a>
+      {props.explore_link && !props.internal_link ? (
+        <a href={props.explore_link} className="teaser__link text-cta">
+          Explore
+        </a>
+      ) : (
+        ""
+      )}
+      {props.explore_link && props.internal_link ? (
+        <a
+          onClick={(e) => {
+            e.preventDefault();
+            dispatch({ type: "SET_NAV", payload: `/${props.read_more}` });
+            router.push(`/${props.read_more}`);
+          }}
+          href={props.explore_link}
+          className="teaser__link text-cta"
+        >
+          Explore
+        </a>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
--- data-loader/get-nodes.ts
+++ data-loader/get-nodes.ts
@@ -6,7 +6,7 @@ function loadFilesAndParse(basePath: string, files: Array<string>) {
         .map(fileLoaded => JSON.parse(fileLoaded.toString())).flat()
 }
 
-function findSlides(pages: Array<any>, nodes: Array<any>, actual: any, lastNode: any, nextNode: any, nodeBase:any) {
+function findSlides(pages: Array<any>, nodes: Array<any>, actual: any, lastNode: any, nextNode: any, nodeBase: any) {
     return pages.map(page => {
         const nodeFinded = nodes.find(node => page === node.id)
         return actual.id !== nodeFinded.id ? {
@@ -26,7 +26,7 @@ function findSlides(pages: Array<any>, nodes: Array<any>, actual: any, lastNode:
                 view: [
                     {
                         component: "CarouselItem",
-                        props: Object.assign({...nodeFinded.page_props}, {active:true})
+                        props: Object.assign({ ...nodeFinded.page_props }, { active: true })
                     }
 
                 ].concat(nodeFinded.components)
@@ -38,7 +38,7 @@ function findSlides(pages: Array<any>, nodes: Array<any>, actual: any, lastNode:
     })
 }
 
-function prepareMenu(nodes: Array<any>, baseNode:any) {
+function prepareMenu(nodes: Array<any>, baseNode: any) {
     const mainMenu = loadFilesAndParse('./data/menu', fs.readdirSync(path.join('./data/menu'))
         .filter(value => value.startsWith('main'))
         .filter(value => value.endsWith('.json')))
@@ -55,17 +55,17 @@ function prepareMenu(nodes: Array<any>, baseNode:any) {
                     component: "SchoolsMenu",
                     props: {
                         title: "Select school",
-                        options_schools: schoolMenu[0].schools.map((value:any) => {
+                        options_schools: schoolMenu[0].schools.map((value: any) => {
                             const nodeFind = nodes.find(node => value.id === node.id);
-                            const link = baseNode.id === nodeFind.id ? `${baseNode.path}` :  `${baseNode.path}/${nodeFind.path}`
+                            const link = baseNode.id === nodeFind.id ? `${baseNode.path}` : `${baseNode.path}/${nodeFind.path}`
                             return {
                                 title: value.title ? value.title : nodeFind.page_props.title,
                                 link_to: '/' + link
                             }
                         }),
-                        options_units: schoolMenu[0].units.map((value:any) => {
+                        options_units: schoolMenu[0].units.map((value: any) => {
                             const nodeFind = nodes.find(node => value.id === node.id);
-                            const link = baseNode.id === nodeFind.id ? `${baseNode.path}` :  `${baseNode.path}/${nodeFind.path}`
+                            const link = baseNode.id === nodeFind.id ? `${baseNode.path}` : `${baseNode.path}/${nodeFind.path}`
                             return {
                                 title: value.title ? value.title : nodeFind.page_props.title,
                                 link_to: '/' + link
@@ -77,9 +77,9 @@ function prepareMenu(nodes: Array<any>, baseNode:any) {
                     component: "MainMenu",
                     props: {
                         title: "Menu",
-                        options: mainMenu[0].links.map((link:any) => {
-                            const nodeFind = nodes.find(node =>  link.id  === node.id)
-                            const linkFound = baseNode.id === nodeFind.id ? `${baseNode.path}` :  `${baseNode.path}/${nodeFind.path}`
+                        options: mainMenu[0].links.map((link: any) => {
+                            const nodeFind = nodes.find(node => link.id === node.id)
+                            const linkFound = baseNode.id === nodeFind.id ? `${baseNode.path}` : `${baseNode.path}/${nodeFind.path}`
 
                             return {
                                 title: link.title ? link.title : nodeFind.page_props.title,
@@ -94,9 +94,9 @@ function prepareMenu(nodes: Array<any>, baseNode:any) {
     }
 }
 
-function prepareBottomMenu(lastNode: any, nextNode: any, nodes: Array<any>, baseNode:any) {
+function prepareBottomMenu(lastNode: any, nextNode: any, nodes: Array<any>, baseNode: any) {
     const basePath = baseNode ? `${baseNode.path}/` : '';
-    const prevNodeSelect = !lastNode ? nodes[nodes.length-1] : lastNode;
+    const prevNodeSelect = !lastNode ? nodes[nodes.length - 1] : lastNode;
     const nextNodeSelect = !nextNode ? nodes[0] : nextNode;
     const previous_route = prevNodeSelect.id === baseNode.id ? `${baseNode.path}` : `${baseNode.path}/${prevNodeSelect.path}`;
     const next_route = nextNodeSelect.id === baseNode.id ? `${baseNode.path}` : `${baseNode.path}/${nextNodeSelect.path}`;
@@ -106,7 +106,7 @@ function prepareBottomMenu(lastNode: any, nextNode: any, nodes: Array<any>, base
             "props": {
                 "previous_title": prevNodeSelect?.page_props.header,
                 "next_title": nextNodeSelect?.page_props?.header,
-                "previous_route": '/'+ previous_route,
+                "previous_route": '/' + previous_route,
                 "next_route": '/' + next_route
             }
         },
@@ -117,31 +117,43 @@ function prepareBottomMenu(lastNode: any, nextNode: any, nodes: Array<any>, base
     ]
 }
 
-function generatePageWithComponents(pages_list: {list:Array<string>, nodeBase:any}, nodes: Array<any>) {
+function generatePageWithComponents(pages_list: { list: Array<string>, nodeBase: any }, nodes: Array<any>) {
     const pages = pages_list.list;
-    const nodesForCollection = pages.map(page => nodes.find(node=> node.id === page));
+    const nodesForCollection = pages.map(page => nodes.find(node => node.id === page));
     return pages.map((page, i) => {
         const nodeFinded = nodes.find(node => page === node.id);
-        const prevNode = i === 0 ? nodes.find(node => node.id === pages[pages.length -1]) : nodes.find(node => node.id === pages[i - 1]);
+        const prevNode = i === 0 ? nodes.find(node => node.id === pages[pages.length - 1]) : nodes.find(node => node.id === pages[i - 1]);
         const nextNode = i === pages.length - 1 ? nodes.find(node => node.id === pages[0]) : nodes.find(node => node.id === pages[i + 1]);
         const menus = prepareMenu(nodesForCollection, pages_list.nodeBase);
         const slides = findSlides(pages, nodesForCollection, nodeFinded, prevNode, nextNode, pages_list.nodeBase);
-        nodeFinded.components.forEach((component:any) =>{
-            if(component.component === 'AccordionComponent'){
+        nodeFinded.components.forEach((component: any) => {
+            if (component.component === 'AccordionComponent') {
                 const re = new RegExp("^(http|https)://", "i");
 
-                component.props.items.forEach((item:any) =>{
-                    if(item.read_more && !re.test(item.read_more)){
-                        const nodeAccordionLink = nodes.find(node => pages.findIndex(page => page === node.id)>-1);
+                component.props.items.forEach((item: any) => {
+                    if (item.read_more && !re.test(item.read_more)) {
+                        const nodeAccordionLink = nodes.find(node => pages.findIndex(page => page === item.read_more && node.id === page) > -1);
                         const path = nodeAccordionLink.id === pages_list.nodeBase.id ? `${pages_list.nodeBase.path}` : `${pages_list.nodeBase.path}/${nodeAccordionLink.path}`;
                         item.read_more = path;
                         item.internal_link = true;
                     }
                 })
             }
-        })
-        if(i === 0 || i === pages.length - 1){
-            if(i===0){
+            if (component.component === 'CampaignCarousel') {
+                const re = new RegExp("^(http|https)://", "i");
+                component.props.slides.forEach((slide: any) => {
+                    const item = slide.props;
+                    if (item.explore_link && !re.test(item.explore_link)) {
+                        const nodeAccordionLink = nodes.find(node => pages.findIndex(page => page === item.explore_link && node.id === page) > -1);
+                        const path = nodeAccordionLink.id === pages_list.nodeBase.id ? `${pages_list.nodeBase.path}` : `${pages_list.nodeBase.path}/${nodeAccordionLink.path}`;
+                        item.explore_link = path;
+                        item.internal_link = true;
+                    }
+                })
+            }
+        });
+        if (i === 0 || i === pages.length - 1) {
+            if (i === 0) {
                 slides.unshift({
                     component: "DynamicComponentMatcher",
                     props: {
@@ -150,12 +162,12 @@ function generatePageWithComponents(pages_list: {list:Array<string>, nodeBase:an
                                 component: "CarouselItem",
                                 props: prevNode.page_props
                             }
-        
+
                         ]
                     }
                 });
-                if(pages.length > 2){
-                    const previousNodeCloned = nodes.find(node => node.id === pages[pages.length -2]);
+                if (pages.length > 2) {
+                    const previousNodeCloned = nodes.find(node => node.id === pages[pages.length - 2]);
                     slides.unshift({
                         component: "DynamicComponentMatcher",
                         props: {
@@ -164,13 +176,13 @@ function generatePageWithComponents(pages_list: {list:Array<string>, nodeBase:an
                                     component: "CarouselItem",
                                     props: previousNodeCloned.page_props
                                 }
-            
+
                             ]
                         }
                     });
                 }
 
-            }else{
+            } else {
                 slides.push({
                     component: "DynamicComponentMatcher",
                     props: {
@@ -179,12 +191,12 @@ function generatePageWithComponents(pages_list: {list:Array<string>, nodeBase:an
                                 component: "CarouselItem",
                                 props: nextNode.page_props
                             }
-        
+
                         ]
                     }
                 });
-                if(pages.length > 2){
-                    const nextNodeCloned = nodes.find(node => node.id === pages[0+1]);
+                if (pages.length > 2) {
+                    const nextNodeCloned = nodes.find(node => node.id === pages[0 + 1]);
                     slides.push({
                         component: "DynamicComponentMatcher",
                         props: {
@@ -193,7 +205,7 @@ function generatePageWithComponents(pages_list: {list:Array<string>, nodeBase:an
                                     component: "CarouselItem",
                                     props: nextNodeCloned.page_props
                                 }
-            
+
                             ]
                         }
                     });
@@ -217,12 +229,12 @@ function generatePageWithComponents(pages_list: {list:Array<string>, nodeBase:an
                                     {
                                         component: "CarouselItem",
                                         // We assign the active prop for scale only this element.
-                                        props: Object.assign({...nodeFinded.page_props}, {active: true})
+                                        props: Object.assign({ ...nodeFinded.page_props }, { active: true })
                                     }
 
                                 ].concat(nodeFinded.components).concat(
                                     prepareBottomMenu(prevNode, nextNode, nodesForCollection, pages_list.nodeBase)
-                                    )
+                                )
                             }
                         },
                         next: !nextNode ? null : (nextNode.id !== pages_list.nodeBase.id ? `${pages_list.nodeBase.path}/${nextNode.path}` : pages_list.nodeBase.path),
@@ -240,6 +252,6 @@ export function getNodes() {
         .filter(value => value.endsWith('.json')))
         ;
     //console.log(JSON.stringify(generatePageWithComponents(pages, nodes)));
-    const pages = nodes.map(node => ({list:node.list, nodeBase:node})).filter(value=> value.list !== undefined);
-    return { paths: pages.map(pages_list=> generatePageWithComponents(pages_list, nodes)).flat() };
+    const pages = nodes.map(node => ({ list: node.list, nodeBase: node })).filter(value => value.list !== undefined);
+    return { paths: pages.map(pages_list => generatePageWithComponents(pages_list, nodes)).flat() };
 }
