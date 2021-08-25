51dc6e77785dd52e08ea94fd5b537628edfb0089
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-08-25 13:38:31 +0200

    Issue #20: Component image text header left or right layout

--- components/TextImageHeader.tsx
+++ components/TextImageHeader.tsx
@@ -9,15 +9,14 @@ import MediaWithExpantion from "./MediaWithExpantion";
 
 export default function TextImageHeader(props:any) {
   return (
-    <div className="section text-image-header">
+    <div className={`section text-image-header ${props.layout === "left" ? "layout-left" : "layout-right"}`}>
         <div className="container">
           <div className="row">
-            <div className="col-md-5 column-media">
-              <MediaWithExpantion img_src={props.img_src} media_src={props.media_src} media_alt={props.media_alt} media_type={props.media_type} size="big" header={props.media_header} text={props.media_text}></MediaWithExpantion>
-            </div>
+            <div className="col-md-6"></div>
             <div className="col-md-6 column-text">
               {props.pretitle && <div className="pretitle text-label">{props.pretitle}</div>}
               <h2 className="title text-body--lg">{props.title}</h2>
+              <MediaWithExpantion img_src={props.img_src} media_src={props.media_src} media_alt={props.media_alt} media_type={props.media_type} size="big" header={props.media_header} text={props.media_text}></MediaWithExpantion>
               <div className="text-image-header__line"></div>
               <div className="body" dangerouslySetInnerHTML={{__html:props.body}}></div>
               {props.read_more && <a href={props.read_more} className="readmore text-cta">Read more</a>}
