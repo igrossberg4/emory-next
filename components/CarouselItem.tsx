03fa88cb1778510b51395ab0c5c1b8c60fbb5c8f
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-09-01 20:07:43 +0200

    CAROUSEL - Code style.

--- components/CarouselItem.tsx
+++ components/CarouselItem.tsx
@@ -17,40 +17,38 @@ export default function CarouselItem(props: any) {
   const multipleSizesImgPrincipal = require(`../public/images/${(props.img_src)}?resize&sizes[]=1024,sizes[]=2048&format=webp&lqip`);
   const memo = useMemo(() => {
     return  <div className="content-header__container container-force-screen-fit-y" data-animation={props.active && goingUp ? "active" : "not-active"}>
-    <div className="header-inner-content">
+      <div className="header-inner-content">
         <div className="header-inner-content__img round-wp">
-          <Image priority={true} src={multipleSizesImgPrincipal.src}  alt={props.header} layout={'fill'}
-
-         ></Image>
+          <Image priority={true} src={multipleSizesImgPrincipal.src} alt={props.header} layout={'fill'}/>
         </div>
         <div className="header-inner-content__text">
           <div className="pretitle text-label">{props.about}</div>
           <h1 className="title header-h2" dangerouslySetInnerHTML={{ __html: props.header }}></h1>
           <div className="subtitle text-body--lg">{props.text}</div>
         </div>
-    </div>
-    <div className="actions">
-      <div
-        className="btn"
-        style={{ cursor: "pointer" }}
-        onClick={(e) => {
-          const contentElement = document.getElementById("carouselContent");
-          //contentElement?.scrollIntoView({behavior: 'smooth'});
-          window.scrollTo({
-            top: 200, //contentElement?.offsetTop,
-            behavior: "smooth",
-          });
-        }}
-      >
-        {" "}
-        {props.button_scroll}
+      </div>
+      <div className="actions">
+        <div
+          className="btn"
+          style={{ cursor: "pointer" }}
+          onClick={(e) => {
+            const contentElement = document.getElementById("carouselContent");
+            //contentElement?.scrollIntoView({behavior: 'smooth'});
+            window.scrollTo({
+              top: 200, //contentElement?.offsetTop,
+              behavior: "smooth",
+            });
+          }}
+        >
+          {" "}
+          {props.button_scroll}
+        </div>
       </div>
     </div>
-  </div>
   }, [goingUp, multipleSizesImgPrincipal, props])
 
   useEffect(() => {
-     const handleScroll = () => {
+    const handleScroll = () => {
       if(props.active){
         //setScroll(window.scrollY);
         const currentScrollY = window.scrollY;
@@ -59,7 +57,7 @@ export default function CarouselItem(props: any) {
         }else{
           setGoingUp(false);
         }
-  
+
         if (currentScrollY >= 25 && !document.body.classList.contains('is-scrolled')) {
           document.body.classList.add('is-scrolled');
         }
@@ -73,6 +71,7 @@ export default function CarouselItem(props: any) {
 
     return () => window.removeEventListener("scroll", handleScroll);
   }, [goingUp]); // @ts-ignore
+
   return (memo
     // <motion.div className="content-header">
    
