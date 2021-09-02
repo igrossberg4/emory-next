f67575d6cd2010be19d0c47a7d4c20d44ed40f0d
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-09-02 16:18:27 +0200

    Disable low image option on the image in the carousel item.

--- components/CarouselItem.tsx
+++ components/CarouselItem.tsx
@@ -14,7 +14,7 @@ const normalize = (val: number, max: number, min: number) => {
 
 export default function CarouselItem(props: any) {
   const [goingUp, setGoingUp] = useState(false);
-  const multipleSizesImgPrincipal = require(`../public/images/${(props.img_src)}?resize&sizes[]=1024,sizes[]=2048&format=webp&lqip`);
+  const multipleSizesImgPrincipal = require(`../public/images/${(props.img_src)}?resize&sizes[]=1024,sizes[]=2048&format=webp`);
   const memo = useMemo(() => {
     return  <div className="content-header__container container-force-screen-fit-y" data-animation={props.active && goingUp ? "active" : "not-active"}>
       <div className="header-inner-content">
