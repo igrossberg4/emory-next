d484b8d84ea4b4d2da650f3de24b64767391f04c
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-09-06 11:29:09 +0200

    Page college art & sciences

--- components/TextLargeImage.tsx
+++ components/TextLargeImage.tsx
@@ -11,12 +11,14 @@ export default function TextLargeImage(props:any) {
   return (
     <div className="section text-large-image" style={{backgroundImage:props.background_image ? `linear-gradient(0deg, #F5F4F5, #F5F4F5), url(/${props.background_image})` : ''}}>
         <div className="container">
+        {props.header && 
           <div className="row header-container">
             <div className="col-md-6"></div>
             <div className="col-md-6">
               <h2 className="title header-h3">{props.header}</h2>
             </div>
           </div>
+          }
           <div className="row body-container">
             <div className="col-md-6">
               <div className="body" dangerouslySetInnerHTML={{__html:props.text}}></div>
