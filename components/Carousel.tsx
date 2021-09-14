a00af7d85096a00186cd4afd6008b553ef2f5c64
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-09-14 14:45:32 +0200

    Carousel  arrows used changed arrows icons

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -29,8 +29,8 @@ export const PrevButton = ({ enabled, onClick, href }: ButtonEnabled) => (
     onClick={onClick}
     disabled={href != undefined ? false : true}
   >
-    <svg className="embla__button__svg" viewBox="137.718 -1.001 366.563 644">
-      <path d="M428.36 12.5c16.67-16.67 43.76-16.67 60.42 0 16.67 16.67 16.67 43.76 0 60.42L241.7 320c148.25 148.24 230.61 230.6 247.08 247.08 16.67 16.66 16.67 43.75 0 60.42-16.67 16.66-43.76 16.67-60.42 0-27.72-27.71-249.45-249.37-277.16-277.08a42.308 42.308 0 0 1-12.48-30.34c0-11.1 4.1-22.05 12.48-30.42C206.63 234.23 400.64 40.21 428.36 12.5z" />
+    <svg className="embla__button__svg" width="9" height="16" viewBox="0 0 9 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M7.54267 15.2093L0 7.66668L7.54267 0.124008L8.48533 1.06668L1.88533 7.66668L8.48533 14.2667L7.54267 15.2093Z" fill="#212322"/>
     </svg>
   </button>
 );
@@ -41,8 +41,8 @@ export const NextButton = ({ enabled, onClick, href }: ButtonEnabled) => (
     onClick={onClick}
     disabled={href != undefined ? false : true}
   >
-    <svg className="embla__button__svg" viewBox="0 0 238.003 238.003">
-      <path d="M181.776 107.719L78.705 4.648c-6.198-6.198-16.273-6.198-22.47 0s-6.198 16.273 0 22.47l91.883 91.883-91.883 91.883c-6.198 6.198-6.198 16.273 0 22.47s16.273 6.198 22.47 0l103.071-103.039a15.741 15.741 0 0 0 4.64-11.283c0-4.13-1.526-8.199-4.64-11.313z" />
+    <svg className="embla__button__svg" width="9" height="16" viewBox="0 0 9 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0.942667 15.2093L8.48533 7.66668L0.942667 0.124008L0 1.06668L6.6 7.66668L0 14.2667L0.942667 15.2093Z" fill="#212322"/>
     </svg>
   </button>
 );
@@ -111,7 +111,7 @@ export default function EmblaCarousel({
             return;
         }
       }
-      
+
     };
     document.body.addEventListener("keydown", handleKey, { passive: false });
     return () =>  document.body.removeEventListener("keydown", handleKey);
@@ -120,13 +120,13 @@ export default function EmblaCarousel({
   return (
     <>
     <div
-      className={`embla embla--carousel-navigation 
-      ${!navigation ? "page-carousel" : ""} 
+      className={`embla embla--carousel-navigation
+      ${!navigation ? "page-carousel" : ""}
       ${index !== page? 'transitioning' : ''}`}
     >
         <div ref={refViewport} className="embla__viewport" key={"viewPort"}>
           <div
-          
+
           >
               <div
                 className="embla__container"
@@ -153,7 +153,7 @@ export default function EmblaCarousel({
                       className={`embla_slide_present ${
                         page === i ? "selected" : "no_selected"
                       } ${i < page ? "first" : ""} ${i > page ? "last" : ""} `}
-                     
+
                       //style={{transform:`translateX(${page === i ? 0 :(page > i ? 100 : -100)}px)`}}
                     >
                       <div
