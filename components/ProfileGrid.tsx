787ea7b8c9c62a8ad0dea2518e210f8dc34b3dd0
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-10-04 15:28:08 +0200

    EAEC-7: Add ProfileGrid component

---
+++ components/ProfileGrid.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import { imageLoader } from "./utils/imageLoader";
+
+export default function ProfileGrid(props:any) {
+
+  return (
+    <div className="section component-profile-grid">
+      <div className="container">
+        {
+          props.header ?
+            <div className="row header-container">
+              <h2 className={ props.size == 'small' ? "" : "header-h1"}>{props.header}</h2>
+            </div> : ''
+        }
+        <div className="row body-container">
+          {
+            props.items.map((item: any, index: number) => {
+              const multipleSizesImgPrincipal = item.img_src ? require(`../public/images/${(item.img_src)}?resize&sizes[]=300&format=png`) : undefined;
+
+              return (
+                <div key={index} className={`profile-grid-item col-sm-6 col-lg-4 col-xl-3 ${ props.size == 'small' ? 'profile-grid-item--small' : '' }`}>
+                  <div className="profile-grid-item__image">
+                    <Image loader={imageLoader(multipleSizesImgPrincipal) as any} alt={item.media_alt} width={300} height={300} src={multipleSizesImgPrincipal.src}></Image>
+                  </div>
+                  <div className="profile-grid-item__name">{ item.name }</div>
+                  { item.description ?
+                    <div className="profile-grid-item__description">{ item.description }</div>
+                  : '' }
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
