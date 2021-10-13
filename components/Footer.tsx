99014c5acfbd7c0d2d748698f18cd8c0a1778c00
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-10-13 14:20:00 -0500

    EAEC-35: Add fallback text and email to footer contact

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,63 +26,118 @@ export default function Footer(props:any) {
   return (
     <footer role="contentinfo">
       <div className="container">
-        {scroll > 170 ?
-        <div
-          className="footer__scrolltop btn text-label"
-          style={{ cursor: "pointer" }}
-          onClick={(e) => {
-            window.scrollTo({
-              top: 0,
-              behavior: "smooth",
-            });
-            setTimeout(() => {
-              document.body.classList.remove("is-scrolled");
-              dispatch({ type: "IS_TRANSITIONING", payload: true });
-              dispatch({
-                type: "GOING_UP",
-                payload: false,
+        {scroll > 170 ? (
+          <div
+            className="footer__scrolltop btn text-label"
+            style={{ cursor: "pointer" }}
+            onClick={(e) => {
+              window.scrollTo({
+                top: 0,
+                behavior: "smooth",
               });
-              console.log("circleAnimateCollapse");
               setTimeout(() => {
-                dispatch({ type: "IS_TRANSITIONING", payload: false });
-              }, 600);
-              const element = document.getElementById("selected");
-              if (element) {
-                const activeElement = element.querySelector(".content-header__container");
-                activeElement?.setAttribute("data-animation", "no-active");
-              }
-              const elementHeader =  document.getElementById('header');
-              if(element){
-                element.classList.remove('hide');
-              }
-            }, 400)
-          }}
-        >
-          <IconButton icon="chevron-up"></IconButton>
-          Back to top
-        </div> : ''
-}
+                document.body.classList.remove("is-scrolled");
+                dispatch({ type: "IS_TRANSITIONING", payload: true });
+                dispatch({
+                  type: "GOING_UP",
+                  payload: false,
+                });
+                console.log("circleAnimateCollapse");
+                setTimeout(() => {
+                  dispatch({ type: "IS_TRANSITIONING", payload: false });
+                }, 600);
+                const element = document.getElementById("selected");
+                if (element) {
+                  const activeElement = element.querySelector(
+                    ".content-header__container"
+                  );
+                  activeElement?.setAttribute("data-animation", "no-active");
+                }
+                const elementHeader = document.getElementById("header");
+                if (element) {
+                  element.classList.remove("hide");
+                }
+              }, 400);
+            }}
+          >
+            <IconButton icon="chevron-up"></IconButton>
+            Back to top
+          </div>
+        ) : (
+          ""
+        )}
         <p className="footer__title">The future starts here</p>
         <div className="footer__cta">
-          <Link  href={props?.cta_support_button?.url ? props?.cta_support_button?.url : "https://together.emory.edu/give"}>
-            <a className="link-button">{props?.cta_support_button?.text ? props?.cta_support_button?.text : 'Support Emory'}</a>
+          <Link
+            href={
+              props?.cta_support_button?.url
+                ? props?.cta_support_button?.url
+                : "https://together.emory.edu/give"
+            }
+          >
+            <a className="link-button">
+              {props?.cta_support_button?.text
+                ? props?.cta_support_button?.text
+                : "Support Emory"}
+            </a>
           </Link>
-          {!props?.cta_contact_info?.raw_block ? 
-          <div className="footer__contact">
-            <div className="footer__contact__heading">{props?.cta_contact_info?.heading ? props?.cta_contact_info?.heading : '' }</div>
-            <div className="footer__contact__person">{props?.cta_contact_info?.person ? props?.cta_contact_info?.person : '' }</div>
-            <div className="footer__contact__title">{props?.cta_contact_info?.title ? props?.cta_contact_info?.title : '' }</div>
-            <div className="footer__contact__links">
-              <a href={`mailto: ${props?.cta_contact_info?.email ? props?.cta_contact_info?.email : ''}` }>{props?.cta_contact_info?.email ? props?.cta_contact_info?.email : '' }</a>
-              <a href={`tel:${props?.cta_contact_info?.telephone ? props?.cta_contact_info?.telephone : ''}` }>{props?.cta_contact_info?.telephone ? props?.cta_contact_info?.telephone : '' }</a>
+          {!props?.cta_contact_info?.raw_block ? (
+            <div className="footer__contact">
+              <div className="footer__contact__heading">
+                {props?.cta_contact_info?.heading
+                  ? props?.cta_contact_info?.heading
+                  : "Learn more about ways to support 2036"}
+              </div>
+              <div className="footer__contact__person">
+                {props?.cta_contact_info?.person
+                  ? props?.cta_contact_info?.person
+                  : ""}
+              </div>
+              <div className="footer__contact__title">
+                {props?.cta_contact_info?.title
+                  ? props?.cta_contact_info?.title
+                  : ""}
+              </div>
+              <div className="footer__contact__links">
+                <a
+                  href={`mailto: ${
+                    props?.cta_contact_info?.email
+                      ? props?.cta_contact_info?.email
+                      : ""
+                  }`}
+                >
+                  {props?.cta_contact_info?.email
+                    ? props?.cta_contact_info?.email
+                    : "support2O36@emory.edu"}
+                </a>
+                <a
+                  href={`tel:${
+                    props?.cta_contact_info?.telephone
+                      ? props?.cta_contact_info?.telephone
+                      : ""
+                  }`}
+                >
+                  {props?.cta_contact_info?.telephone
+                    ? props?.cta_contact_info?.telephone
+                    : ""}
+                </a>
+              </div>
             </div>
-          </div>
-
-: <div className="footer__contact" dangerouslySetInnerHTML={{__html:props?.cta_contact_info?.raw_block}}>
-  
-  </div>}
+          ) : (
+            <div
+              className="footer__contact"
+              dangerouslySetInnerHTML={{
+                __html: props?.cta_contact_info?.raw_block,
+              }}
+            ></div>
+          )}
         </div>
-        <Image src="/logos/emory-university-logo.svg" alt="EMORY" width="95px" height="20px"></Image>
+        <Image
+          src="/logos/emory-university-logo.svg"
+          alt="EMORY"
+          width="95px"
+          height="20px"
+        ></Image>
       </div>
     </footer>
   );
