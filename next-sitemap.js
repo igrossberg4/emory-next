9470b0286549259a6c68f6a1eeac49d72db8fb5d
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-10-14 10:14:10 +0200

    EAEC-45: Generate sitemap.xml with next-sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://example.com",
+  changefreq: "weekly",
+  outDir: "out/",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
