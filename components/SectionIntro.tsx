165c5b11296c9d3d08e632f09de6fe71ecff17fe
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-10-14 16:39:47 -0500

    EAEC-40: Vertically center text in intro component

--- components/SectionIntro.tsx
+++ components/SectionIntro.tsx
@@ -11,22 +11,44 @@ export default function SectionIntro(props:any) {
   const multipleSizesImgPrincipal = props.background_image ? require(`../public/images/${(props.background_image)}?resize&sizes[]=2048&format=png`) : undefined;
 
   return (
-    <div className="section component-intro-text" style={{backgroundImage:props.background_image ?  `url(${multipleSizesImgPrincipal.src})` : ''}}>
-        <div className="container">
-          <div className="row header-container">
-            <div className="col-md-6">
-                <h2 className="text-body--lg"  dangerouslySetInnerHTML={{__html:props.header}} ></h2>
-            </div>
-          </div>
-          <div className="row body-container">
-            <div className="col-md-8 image-container">
-              <MediaWithExpantion img_src={props.img_src} media_src={props.media_src} media_alt={props.media_alt} media_type={props.media_type} size="big" header={props.media_header} text={props.media_text} byline={props.media_byline}  ></MediaWithExpantion>
-            </div>
-            <div className="col-md-6">
-              <div className="body" dangerouslySetInnerHTML={{__html:props.text}}></div>
-            </div>
+    <div
+      className='section component-intro-text'
+      style={{
+        backgroundImage: props.background_image
+          ? `url(${multipleSizesImgPrincipal.src})`
+          : "",
+      }}
+    >
+      <div className='container'>
+        <div className='row header-container'>
+          <div className='col-md-6'>
+            <h2
+              className='text-body--lg'
+              dangerouslySetInnerHTML={{ __html: props.header }}
+            ></h2>
           </div>
         </div>
+        <div className='row body-container'>
+          <div className='col-md-8 image-container'>
+            <MediaWithExpantion
+              img_src={props.img_src}
+              media_src={props.media_src}
+              media_alt={props.media_alt}
+              media_type={props.media_type}
+              size='big'
+              header={props.media_header}
+              text={props.media_text}
+              byline={props.media_byline}
+            ></MediaWithExpantion>
+          </div>
+          <div className='col-md-6 text-container'>
+            <div
+              className='body'
+              dangerouslySetInnerHTML={{ __html: props.text }}
+            ></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
