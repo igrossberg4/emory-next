40fa47289d79a27f66a02a24750a259313693bed
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-11-02 12:51:49 +0100

    EAEC-76 - Added video caption logic. Need style

--- components/Video.tsx
+++ components/Video.tsx
@@ -31,52 +31,120 @@ export default function Video(props: any) {
   return (
     <Fragment>
       {!re.test(media_src) ? (
-        <video
-          disableRemotePlayback={true}
-          playsInline={props.playsInline}
-          muted={props.muted}
-          autoPlay={props.autoPlay}
-          loop={props.loop}
-          onPlay={() => {
-            if (props.onPlay) {
-              props.onPlay();
-            }
-          }}
-          ref={(ref) => {
-            videoRef.current = ref as any;
-            /*dispatch({
+        <Fragment>
+          <video
+            disableRemotePlayback={true}
+            playsInline={props.playsInline}
+            muted={props.muted}
+            autoPlay={props.autoPlay}
+            loop={props.loop}
+            onPlay={() => {
+              if (props.onPlay) {
+                props.onPlay();
+              }
+            }}
+            ref={(ref) => {
+              videoRef.current = ref as any;
+              const tracks = ref?.textTracks[0];
+              const element = document.getElementById("caption-block");
+              if (tracks) {
+                tracks.mode = "hidden"; // must occur before cues is retrieved
+                const cues = tracks.cues;
+                const replaceText = function (text: string) {
+                    if (element) {
+                      element.innerHTML = text;
+                    }
+                  },
+                  showText = function () {
+                    if (element) {
+                      element.style.visibility = "visible";
+                    }
+                  },
+                  hideText = function () {
+                    if (element) {
+                      element.style.visibility = "hidden";
+                    }
+                  },
+                  cueEnter = function (this: any) {
+                    replaceText((this as any).text);
+                    showText();
+                  },
+                  cueExit = function () {
+                    hideText();
+                  },
+                  videoLoaded = function (e: Event) {
+                    if (cues) {
+                      for (let i = 0; i < cues.length; i++) {
+                        const cue = cues[Number(i)];
+                        if (cue) {
+                          cue.onenter = cueEnter;
+                          cue.onexit = cueExit;
+                        }
+                      }
+                    }
+                  };
+
+                ref?.addEventListener("loadedmetadata", videoLoaded);
+              }
+
+              /*dispatch({
               type: "REGISTER_VIDEO",
               payload: {
                 key: props.video_src ? props.video_src : props.media_src,
                 value: { paused: true, muted: true, videoRef: ref, controls:controls },
               },
             });*/
-            if (props.onVideoRef) {
-              props.onVideoRef(ref);
-            }
-            //props.onVideoRef(controls)
-          }}
-          onEnded={(en) => {
-            //dispatch({ type: 'SKIP_VIDEO', payload:{key:props.video_src ? props.video_src : props.media_src}});
-            if (props.onVideoEnd) {
-              props.onVideoEnd();
-            }
-            //router.push(props.route_to)
-            // Check if the video has ended.
-            // TO DO define if we take some action on end.
-          }}
-          controls={props.controls}
-          key={props.video_src ? props.video_src : props.media_src}
-          //initial={!props.initial_animation ? { opacity: 0 } : props.initial_animation}
-          //exit={!props.initial_animation ? { opacity: 1, height: "400px", width: "400px", borderRadius:'50%' } : props.initial_animation}
-        >
-          {props.video_webm_src ? (
-            <source src={props.video_webm_src} type="video/webm"></source>
-          ) : (
-            ""
-          )}
-          <source src={media_src} type="video/mp4"></source>
-        </video>
+              if (props.onVideoRef) {
+                props.onVideoRef(ref);
+              }
+              //props.onVideoRef(controls)
+            }}
+            onEnded={(en) => {
+              //dispatch({ type: 'SKIP_VIDEO', payload:{key:props.video_src ? props.video_src : props.media_src}});
+              if (props.onVideoEnd) {
+                props.onVideoEnd();
+              }
+              //router.push(props.route_to)
+              // Check if the video has ended.
+              // TO DO define if we take some action on end.
+            }}
+            controls={props.controls}
+            key={props.video_src ? props.video_src : props.media_src}
+            //initial={!props.initial_animation ? { opacity: 0 } : props.initial_animation}
+            //exit={!props.initial_animation ? { opacity: 1, height: "400px", width: "400px", borderRadius:'50%' } : props.initial_animation}
+          >
+            {props.video_webm_src ? (
+              <source src={props.video_webm_src} type="video/webm"></source>
+            ) : (
+              ""
+            )}
+            <source src={media_src} type="video/mp4"></source>
+            {state.captionVideoEnabled ? (
+              <track
+                default
+                src={props.video_vtt}
+                kind="captions"
+                srcLang={props.video_vtt_lang}
+                label="english_captions"
+              ></track>
+            ) : (
+              ""
+            )}
+          </video>
+
+          <div
+            className={`${
+              !state.videoPlayed ? "caption-visible" : "caption-hide"
+            }`}
+            style={{
+              position: "fixed",
+              zIndex: 200,
+              bottom: 20,
+              left: "40vw",
+            }}
+            id="caption-block"
+          ></div>
+        </Fragment>
       ) : (
         <div className="video-vimeo">
           <ReactPlayer playing={true} light={false} controls url={media_src} />
