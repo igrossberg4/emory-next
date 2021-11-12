826ded65d41e9c1c0f17b59b06e1f66a47fc15b2
Isaac Grossberg <isaac.grossberg4@gmail.com>
2021-11-12 11:19:38 +0100

    EAEC-90: Preoptimize images

--- scripts/preoptimize-images.js
+++ scripts/preoptimize-images.js
@@ -1,10 +1,30 @@
+// -----------------------------------------------------------------------------
+// DEPENDENCIES AND CONFIG
+
+// File and path node.js tools
 const fs = require('fs');
 const path = require('path');
+
+// Image optimizer and processor
 const sharp = require('sharp');
+
+// Glob patterns for file loading
 const glob = require('glob');
+
+// CLI outpput formatter helper
 const chalk = require('chalk');
 
-console.log(fs.readdirSync('./out/images'))
+// CLI flags and options helper
+const minimist = require('minimist');
+
+// Get the target folder from args;
+const args = minimist(process.argv);
+const targetFolder = args.folder ? args.folder : false;
+const quality = typeof args.quality === 'number' ? args.quality : 90;
+const maxWidth = typeof args.maxwidth === 'number' ? args.maxWidth : 3000;
+
+// -----------------------------------------------------------------------------
+// IMAGE PROCESSING
 
 glob('./public/**/*+(.png|.jpg)', function(error, files){
   if (error) {
@@ -16,27 +36,54 @@ glob('./public/**/*+(.png|.jpg)', function(error, files){
   files.forEach((file) => {
     const fileParts = path.parse(file);
     const fileBuffer =  fs.readFileSync(file);
+    const outFolder = targetFolder ? `./${targetFolder}${fileParts.dir.replace('./public', '') }` : fileParts.dir;
+
+    if(fileBuffer.toString() === '') {
+      console.log(chalk.yellow(`⚠  Empty buffer on ${file}`));
+      return;
+    }
+
+    // Check the existence of the target directory and, if not, create it.
+    if (!fs.existsSync(outFolder)) {
+      fs.mkdirSync(outFolder, { recursive: true });
+      console.log(chalk.yellow(`⚠  Created ${outFolder} folder`));
+    }
 
     if (fileParts.ext == '.jpg') {
       sharp(fileBuffer)
-        .resize(300)
+        .resize(maxWidth, null, {
+          withoutEnlargement: true
+        })
         .jpeg({
-          quality: 90,
+          quality: quality,
           mozjpeg: true
         })
-        .toFile(`./processed/${fileParts.base}`);
+        .toFile(`${outFolder}/${fileParts.base}`)
+        .then(info => {
+          console.log(chalk.green(`🖒  Processed ${outFolder}/${fileParts.base}`));
+        })
+        .catch(error => {
+          console.log(chalk.red.bold(error))
+        });
     } else if (fileParts.ext == '.png') {
-
+      sharp(fileBuffer)
+        .resize(maxWidth, null, {
+          withoutEnlargement: true
+        })
+        .png({
+          quality: quality,
+          compressionLevel: 9,
+        })
+        .toFile(`${outFolder}/${fileParts.base}`)
+        .then(info => {
+          console.log(chalk.green(`🖒  Processed ${outFolder}/${fileParts.base}`));
+        })
+        .catch(error => {
+          console.log(chalk.red.bold(error))
+        });
     } else {
       console.log(chalk.yellow.bold(`⚠  Warning! File with unprocessable extension ${fileParts.ext} was found on the asset list.`))
     }
-
-
-    // sharp(fileBuffer)
-    //   .resize(500)
-    //   .webp({quality:100})
-    //   //.resize(320, 240)
-    //   .toFile('./out/images/mobile-'+image);
   });
 
 })
