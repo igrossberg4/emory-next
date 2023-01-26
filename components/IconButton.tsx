2eb55d7c3e7a10bfed91bac85b2a9e6224a94840
Isaac Grossberg <isaac.grossberg4@gmail.com>
2023-01-25 23:29:43 -0500

    add gift icon

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -1,19 +1,35 @@
 import Link from "next/link";
-import Icons from "./Icons"
+import Icons from "./Icons";
 
 function IconButton(props: any) {
-    return props.link ?
-        props.target ?
-        <a href={props.link} className="icon-button" target={props.target} title={props.title} aria-label={props.label} >
-            <Icons icon={props.icon} />
-        </a>:
-        <Link href={props.link}>
+  return props.link ? (
+    props.target ? (
+      <a
+        href={props.link}
+        className="icon-button"
+        target={props.target}
+        title={props.title}
+        aria-label={props.label}
+      >
+        <Icons icon={props.icon} />
+      </a>
+    ) : (
+      <Link href={props.link}>
         <a className="icon-button" aria-label={props.label}>
-            <Icons icon={props.icon} />
+          <Icons icon={props.icon} />
         </a>
-        </Link> : <button type="button" className="icon-button" onClick={props.onClick} aria-label={props.label}>
-            <Icons icon={props.icon} />
-        </button>
+      </Link>
+    )
+  ) : (
+    <button
+      type="button"
+      className={`icon-button ${props.fixed ? "fixed" : ""}`}
+      onClick={props.onClick}
+      aria-label={props.label}
+    >
+      <Icons icon={props.icon} />
+    </button>
+  );
 }
 
 export default IconButton;
